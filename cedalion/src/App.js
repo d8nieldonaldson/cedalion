d3a261cbfb993ed41d60efe381df0bc2e9271381
Daniel Donaldson <danieldonaldson@Daniels-MacBook-Pro.local>
2020-03-21 17:44:49 -0700

    countdown prettier eslint

---
+++ cedalion/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Countdown from "react-countdown";
+
+// Random component
+const Completionist = () => <span>You are good to go!</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    );
+  }
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Countdown date={Date.now() + 5000} renderer={renderer} />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
